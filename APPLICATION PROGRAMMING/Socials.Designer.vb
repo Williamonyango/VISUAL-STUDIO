c62330447a4614fe0df80eb33a4a46b456d9b6ff
User <User@DESKTOP-OR85G4C>
2024-10-28 07:02:43 +0300

    updated socials

--- APPLICATION PROGRAMMING/Socials.Designer.vb
+++ APPLICATION PROGRAMMING/Socials.Designer.vb
@@ -22,15 +22,62 @@ Partial Class Socials
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.ForestGreen
+        Button1.Font = New Font("Segoe UI", 15F)
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(193, 50)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(274, 97)
+        Button1.TabIndex = 0
+        Button1.Text = "Go to Website"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.ForestGreen
+        Button2.Font = New Font("Segoe UI", 15F)
+        Button2.ForeColor = Color.White
+        Button2.Location = New Point(193, 161)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(274, 97)
+        Button2.TabIndex = 1
+        Button2.Text = "@DiscoverJKUAT"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.ForestGreen
+        Button3.Font = New Font("Segoe UI", 15F)
+        Button3.ForeColor = Color.White
+        Button3.Location = New Point(193, 286)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(274, 97)
+        Button3.TabIndex = 2
+        Button3.Text = "DiscoverJKUAT"
+        Button3.UseVisualStyleBackColor = False
         ' 
         ' Socials
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1095, 400)
+        ClientSize = New Size(1564, 667)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Margin = New Padding(4, 5, 4, 5)
         Name = "Socials"
         Text = "Socials"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- APPLICATION PROGRAMMING/Socials.vb
+++ APPLICATION PROGRAMMING/Socials.vb
@@ -1,3 +1,22 @@
 ﻿Public Class Socials
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim psi As New ProcessStartInfo
+        psi.FileName = "https://www.jkuat.ac.ke/"
+        psi.UseShellExecute = True
+        Process.Start(psi)
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim psi As New ProcessStartInfo
+        psi.FileName = "https://x.com/DiscoverJKUAT"
+        psi.UseShellExecute = True
+        Process.Start(psi)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim psi As New ProcessStartInfo
+        psi.FileName = "https://www.facebook.com/DiscoverJKUAT/"
+        psi.UseShellExecute = True
+        Process.Start(psi)
+    End Sub
 End Class
