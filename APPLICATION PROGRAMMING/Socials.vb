b1390c4b4552ebbc4fc618c2f690aae5f5d7faac
User <User@DESKTOP-OR85G4C>
2024-10-28 08:03:36 +0300

    updated socials

--- APPLICATION PROGRAMMING/Socials.vb
+++ APPLICATION PROGRAMMING/Socials.vb
@@ -1,24 +1,4 @@
 ﻿Public Class Socials
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-        Dim psi As New ProcessStartInfo
-        psi.FileName = "https://www.jkuat.ac.ke/"
-        psi.UseShellExecute = True
-        Process.Start(psi)
-    End Sub
-
-    Private Sub Button2_Click(sender As Object, e As EventArgs)
-        Dim psi As New ProcessStartInfo
-        psi.FileName = "https://x.com/DiscoverJKUAT"
-        psi.UseShellExecute = True
-        Process.Start(psi)
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs)
-        Dim psi As New ProcessStartInfo
-        psi.FileName = "https://www.facebook.com/DiscoverJKUAT/"
-        psi.UseShellExecute = True
-        Process.Start(psi)
-    End Sub
 
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         Dim psi As New ProcessStartInfo
@@ -28,8 +8,9 @@
     End Sub
 
     Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
         Dim psi As New ProcessStartInfo
-        psi.FileName = "https://x.com/DiscoverJKUAT"
+        psi.FileName = "https://www.jkuat.ac.ke/"
         psi.UseShellExecute = True
         Process.Start(psi)
     End Sub
@@ -37,6 +18,27 @@
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
         Dim psi As New ProcessStartInfo
         psi.FileName = "https://www.facebook.com/DiscoverJKUAT/"
+        psi.UseShellExecute = True
+        Process.Start(psi)
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Dim psi As New ProcessStartInfo
+        psi.FileName = "https://x.com/DiscoverJKUAT"
+        psi.UseShellExecute = True
+        Process.Start(psi)
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Dim psi As New ProcessStartInfo
+        psi.FileName = "https://www.facebook.com/DiscoverJKUAT/"
+        psi.UseShellExecute = True
+        Process.Start(psi)
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Dim psi As New ProcessStartInfo
+        psi.FileName = "https://x.com/DiscoverJKUAT"
         psi.UseShellExecute = True
         Process.Start(psi)
     End Sub
--- APPLICATION PROGRAMMING/obj/Debug/net8.0-windows/APPLICATION PROGRAMMING.AssemblyInfo.vb
+++ APPLICATION PROGRAMMING/obj/Debug/net8.0-windows/APPLICATION PROGRAMMING.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("APPLICATION PROGRAMMING"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1897b21714959598476a08e9b900348cc5cd7799"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcdd347c6d5944ebe4c191c20db1ba750c65f972"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("APPLICATION PROGRAMMING"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("APPLICATION PROGRAMMING"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
